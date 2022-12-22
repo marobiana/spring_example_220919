4b5a8055894382dbed29f235f63067e329141f56
test <test@test.com>
2022-12-22 16:14:51 +0900

    초기 환경 세팅

---
+++ src/main/java/com/example/test/TestController.java
@@ -0,0 +1,21 @@
+package com.example.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+
+	// http://localhost:8080/test1
+	@ResponseBody
+	@RequestMapping("/test1")
+	public String test1() {
+		return "Hello world!!!";
+	}
+}
+
+
+
+
+
