76a38c49b3a380415dfc3a659e5356a1241e7d20
test <test@test.com>
2022-12-22 18:39:48 +0900

    2강 예제1 DB 연동

---
+++ src/main/java/com/example/lesson02/dao/UsedGoodsDAO.java
@@ -0,0 +1,17 @@
+package com.example.lesson02.dao;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.example.lesson02.model.UsedGoods;
+
+@Repository
+public interface UsedGoodsDAO {
+	
+	// input:X  (service(BO)가 아무것도 주지않음)
+	// output:List<UsedGoods>
+	public List<UsedGoods> selectUsedGoodsList();
+}
+
+
