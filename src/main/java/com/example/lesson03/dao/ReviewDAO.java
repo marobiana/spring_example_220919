960b89aa56ce1ad794d0b3e7697b0fd6a69dce45
test <test@test.com>
2022-12-26 18:29:04 +0900

    3강 예제2 인서트문

--- src/main/java/com/example/lesson03/dao/ReviewDAO.java
+++ src/main/java/com/example/lesson03/dao/ReviewDAO.java
@@ -1,5 +1,6 @@
 package com.example.lesson03.dao;
 
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.lesson03.model.Review;
@@ -7,4 +8,19 @@ import com.example.lesson03.model.Review;
 @Repository
 public interface ReviewDAO {
 	public Review selectReview(int id);
+	
+	public int insertReview(Review review);
+	
+	public int insertReviewAsField(
+			// 하나의 맵으로 구성됨(@Param)
+			@Param("storeId") int storeId, 
+			@Param("menu") String menu, 
+			@Param("userName") String userName, 
+			@Param("point") Double point, 
+			@Param("review") String review);
 }
+
+
+
+
+
