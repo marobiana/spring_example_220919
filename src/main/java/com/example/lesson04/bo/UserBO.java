8f0cd00a2690e6981f83ce03f17f882bc6b29a70
test <test@test.com>
2022-12-28 17:06:36 +0900

    4강 예제1

--- src/main/java/com/example/lesson04/bo/UserBO.java
+++ src/main/java/com/example/lesson04/bo/UserBO.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.lesson04.dao.UserDAO;
+import com.example.lesson04.model.User;
 
 @Service
 public class UserBO {
@@ -13,6 +14,10 @@ public class UserBO {
 	
 	public void addUser(String name, String yyyymmdd, String email, String introduce) {
 		userDAO.insertUser(name, yyyymmdd, email, introduce);
+	}
+	
+	public User getLastUser() {
+		return userDAO.selectLastUser();
 	}
 }
 
